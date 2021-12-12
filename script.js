9a75b36d6b47d94e0e4620c64d6a0585acdb5c15
Savannahpolcen <Savannahpolcen@gmail.com>
2021-12-12 17:50:29 -0500

    assigning varibles, setting up functions

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 // Assignment code here
+var passwordfinal = []
+var passwordlength 
+var passwordspecials 
+var passwordlowercase
+var passworduppercase 
+var passwordnumbers 
+
+const alphaLower = "abcdefghijklmnopqrstuvwxyz";
+const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const specChar = "!@#$%^&*()_-~<>?/";
+
+function randNumber() { a = Math.floor(Math.random()*10); return a;}
+function randLowChar() { a = alphaLower[Math.floor(Math.random)() * alphaLower.length)]; return a; };
+function randUpperChar () { a = alphaUpper[Math.floor(Math.random() * alphaUpper.length)]; return a; };
+function randSpcChar () { a = specChar[Math.floor(Math.random() * specChar.length)]; return a; };
+
+
 
 
 // Get references to the #generate element
@@ -6,6 +23,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+    
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 

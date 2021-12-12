6e57b5c8f365766f4b9f0e54f9feab5b30d24115
Savannahpolcen <Savannahpolcen@gmail.com>
2021-12-12 18:54:54 -0500

    fixed extra () , line 20

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
 // Assignment code here
-var passwordfinal = []
-var passwordlength 
-var passwordspecials 
-var passwordlowercase
-var passworduppercase 
-var passwordnumbers 
+var passwordFinal = []
+var passwordLength 
+var passwordSpecials 
+var passwordLowercase
+var passwordUppercase 
+var passwordNumbers 
 const alphaLower = "abcdefghijklmnopqrstuvwxyz";
 const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const specChar = "!@#$%^&*()_-~<>?/";
 
 function randNumber() { a = Math.floor(Math.random() * 10); return a;}
-function randLowChar() { a = alphaLower[Math.floor(Math.random)() * alphaLower.length)]; return a; };
-function randUpperChar () { a = alphaUpper[Math.floor(Math.random() * alphaUpper.length)]; return a; };
-function randSpcChar () { a = specChar[Math.floor(Math.random() * specChar.length)]; return a; };
+function randLowChar() { a = alphaLower[Math.floor(Math.random() * alphaLower.Length)]; return a; };
+function randUpperChar () { a = alphaUpper[Math.floor(Math.random() * alphaUpper.Length)]; return a; };
+function randSpcChar () { a = specChar[Math.floor(Math.random() * specChar.Length)]; return a; };
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 // How will the user interact with the page? by clicking on "generate password btn"
 generateBtn.addEventListener("click", function () {
-    var passwordfinal = [];
-    passwordlength = window.prompt("Please enter the number of random characters you would like to include. (between 8 and 128 are required).");
+    var passwordFinal = [];
+    passwordLength = window.prompt("Please enter the number of random characters you would like to include. (between 8 and 128 are required).");
     
-    if ( passwordlength < 8 || passwordlength > 128 || isNaN(passwordlength)) {
+    if ( passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
         window.alert("Please choose a number between 8 and 128");
-        process.exit();
+        
     }
-    passwordspecials = window.confirm("Do you want to include special characters?")
-    passwordlowercase = window.confirm("Do you want to include any lower case letter?")
-    passworduppercase = window.confirm(" Do you want to include any upper case letters?")
-    passwordnumbers = window.confirm("Do you want to include any numbers?")
+    passwordSpecials = window.confirm("Do you want to include special characters?")
+    passwordLowercase = window.confirm("Do you want to include any lower case letter?")
+    passwordUppercase = window.confirm(" Do you want to include any upper case letters?")
+    passwordNumbers = window.confirm("Do you want to include any numbers?")
 
     var pwrdOptions = []
 
-  if (!pwrdSpecials) { } else pwrdOptions.push("pwrdSpecials");
-  if (!pwrdLowerCase) { } else pwrdOptions.push("pwrdLowerCase");
-  if (!pwrdUpperCase) { } else pwrdOptions.push("pwrdUpperCase");
-  if (!pwrdNumbers) { } else pwrdOptions.push("pwrdNumbers");
+  if (!passwordSpecials) { } else pwrdOptions.push("pwrdSpecials");
+  if (!passwordLowerCase) { } else pwrdOptions.push("pwrdLowerCase");
+  if (!passwordUpperCase) { } else pwrdOptions.push("pwrdUpperCase");
+  if (!passwordNumbers) { } else pwrdOptions.push("pwrdNumbers");
 
- function magicNum() {a = Math.floor(Math.random() * pwrdOptions.length); return a;};
+ function magicNum() {a = Math.floor(Math.random() * pwrdOptions.Length); return a;};
 
-  if (pwrdOptions.length === 0) {
+  if (pwrdOptions.Length === 0) {
     window.alert("Please choose at least one character type.");
     process.exit();
   } else {
-    for (let step = 0; step < pwrdLength; step++) {
+    for (let step = 0; step < passwordLength; step++) {
       var randOption = magicNum();
       
-      if (pwrdOptions[randOption] === 'passwordspecials') {
+      if (pwrdOptions[randOption] === 'passwordSpecials') {
         let char = randSpcChar();
         
         pwrdFinal.push(char);
         
-      } else if (pwrdOptions[randOption] === 'passwordlowercase') {
+      } else if (pwrdOptions[randOption] === 'passwordLowercase') {
         let char = randLowChar();
         
         pwrdFinal.push(char);
       
-      } else if (pwrdOptions[randOption] === 'passworduppercase') {
+      } else if (pwrdOptions[randOption] === 'passwordUppercase') {
         let char = randUppChar();
        
         pwrdFinal.push(char);
       
-      } else if (pwrdOptions[randOption] === 'passwordnumber') {
+      } else if (pwrdOptions[randOption] === 'passwordNumber') {
         let char = randNumber();
         
         pwrdFinal.push(char);
